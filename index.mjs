640fb9c3fe10f4ecdbb1047fa816985a2974bb32
Subhadip Ghosh <ghoshsubhadipghosh@gmail.com>
2022-02-26 18:31:31 +0530

    Initial commit :star:

---
+++ index.mjs
@@ -0,0 +1,20 @@
+'use strict'
+
+import e from 'express'
+import fs from 'node:fs'
+import https from 'node:https'
+import path from 'node:path'
+
+
+const app = e()
+
+const sslPath = path.join(process.cwd(), 'keys')
+
+const opts = {
+  key: fs.readFileSync(path.join(sslPath, 'private.key')).toString('utf-8'),
+  cert: fs.readFileSync(path.join(sslPath, 'certificate.crt')).toString('utf-8'),
+}
+
+const server = https.createServer(opts, app)
+
+server.listen(8443, () => console.log('App is running on *:8443'))
